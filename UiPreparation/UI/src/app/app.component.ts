141ac3ba45ef0bfa118388bfd41367cc3c6732d2
Oğuzhan <oguzhan.can.iletisim@gmail.com>
2021-05-15 20:45:35 +0300

    UI ErrorSolved

--- UiPreparation/UI/src/app/app.component.ts
+++ UiPreparation/UI/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title:String = 'UI';
-  user:String = "Oğuzhan Can";
+  title = 'UI';
 }
--- UiPreparation/UI/src/app/app.module.ts
+++ UiPreparation/UI/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { VideosComponent } from './components/videos/videos.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    VideosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
